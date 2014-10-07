52b8855881c39f266dfd2d79e4e7f21f946bbfec
charlespark <cpark5@yahoo.com>
2014-10-07 11:44:19 -0700

    Daily Commit - Tuesday, October 7 late AM
    Daily Commit for today’s early files

--- Ideas_for_RSpec_Testing_Conditions.rb
+++ Ideas_for_RSpec_Testing_Conditions.rb
@@ -1,58 +1,160 @@
-"RSpec test conditions for each category"
+#"RSpec test conditions for each category"
+require 'rails_helper'
 
-User - refers to the actual SNPPR user
-Categories - refers to the categories that users can place images (web-hosted or personal) and webs (web site links) into
-Images - refers to web-hosted images (i.e. images that are already online and linked from other sites)
-URL - refers to the custom URL generated after a user creates a SNPPR output page.  This URL is specific to each output page
-Personals - refers to locally-hosted images on a user machine that need to be hosted on our end before they go up to the output page
-Webs - refers to the actual web sites and links that can be included on the input page
+#User - refers to the actual SNPPR user
+#Categories - refers to the categories in which users can place images (web-hosted or personal) and webs (web site links)
+#Images - refers to web-hosted images (i.e. images already online and linked from other sites)
+#Link - refers to the custom URL generated after a user creates a SNPPR output page.  This Link is specific to each output page
+#Personals - refers to locally-hosted images on a user machine that need to be hosted on our end before they go up to the output page
+#Webs - refers to the actual web sites and links that can be included on the input page
 
-Categories
-has_many :urls
-Is the User logged in?
-Can pictures and webs be placed into categories?
-Are there picture and webs-specific categories?
+# User
+# :full_name
+# :email
+# :password_digest
+# :username
+# has_many :links, :categories
+# Can the user login?
+# Can the user add images/links to the input page whether logged-in or not?
+
+# Link, formerly known as URL, this is the created shortened URL that is output from the API
+# - Slug (doesn't really apply, since the user cannot fully customize the shortened URL (or can they?)
+# - Title
+# - Needs to be able to be unique, clickable and lead to the correct output page
+# has_many :images
+# belongs_to :users, :categories
+# Does the link lead to the proper output page?
+# Does the link shorten correctly?
+
+# Web, this is the model for the web sites that the API links to (and takes the link from)
+# - Title
+# - Screenshot
+# - Metadata
+# - URL
+# belongs_to :categories
+# Does the URL lead to a working page?
+# Does the URL lead to a safe/suspicious page?
+# Does the URL contain metadata/screenshot that can be scraped?
+
+# Image, this is the web link for images that are shared on the web (i.e. already online)
+# - Link (URL)
+# - Alt text
+# belongs_to :links, :personals
+# describe
+# it "should have an image_path (address)"
+# What categories does an image have?
+# A URL (i.e. www.site.com/fancy.jpg)
+# A filename (fancy.jpg)
+# Image size (file size, pixel size)
+# It should be format JPG
+# What happens if it is PNG / GIF / Other?
+# Does this have alt-text?
+
+# Categories
+# has_many :links
+# Is the User logged in?
+# Can pictures and webs be placed into categories?
+# Are there picture and webs-specific categories?
+
+# Personal
+# has_many :images
+# Can the user upload images from their computer?
+# Not directly related, though can the user drag-and-drop pictures to the input page?
+
+# Sessions(?)
+# Skip this for any model testing
 
 
-Images
-belongs_to :urls, :personals
-describe
-it "should have an image_path (address)"
+describe User do
+	user = User.new
 
-What categories does an image have?
-A URL (i.e. www.site.com/fancy.jpg)
-A filename (fancy.jpg)
-Image size (file size, pixel size)
-It should be format JPG
-What happens if it is PNG / GIF / Other?
-Does this have alt-text?
+	describe 'user test case' do
+		before do
+			@default = User.new(full_name: 'John Doe', email: 'test@abc.com', password_digest: '1234', username: 'test')
+		end
 
+		it "should have a full_name" do
+			expect(@default.full_name = 'John Doe')
+		end
 
-Personals
-has_many :images
-Can the user upload images from their computer?
-Not directly related, though can the user drag-and-drop pictures to the input page?
+		it "should have an email" do
+			expect(@default.email = 'test@abc.com')
+		end
 
+		it "should have a password_digest" do
+			expect(@default.password_digest = '1234')
+		end
 
-Urls
-has_many :images
-belongs_to :users, :categories
-Does the URL lead to the proper output page?
-Does the URL shorten correctly?
-
-
-Users
-has_many :urls, :categories
-Can the user login?
-Can the user add images/links to the input page whether logged-in or not?
+		it "should have a username" do
+			expect(@default.username = 'test')
+		end
+	end
+end
 
 
-Webs
-belongs_to :categories
-Does the link lead to a working page?
-Does the link lead to a safe/suspicious page?
-Does the link contain metadata/screenshot that can be scraped?
+# the below are tests for the controller_spec page
+describe "GET index" do
+		it "should render an account page" do
+			get :account_page
+			expect(response).to render_template(:account_page)
+		end
+
+		it "should return a list of saved links" do
+			get :account_page
+			@links = assigns :links)
+			expect@links.to match_array(Links.all)
+		end
+end
+
+describe "GET show" do
+
+# is this a model or a controller spec test case?
+		it "should be able to create and populate categories" do
+			expect(@default) to create custom categories
+		end
+
+#IF NOT LOGGED IN
+		it "should be able to use functionality as a guest" do
+			expect to have input and output functionality as a guest
+		end
+	end
+end
 
 
-Sessions(?)
-Skip this for any model testing
+describe Categories do
+	category = Category.new
+
+	describe 'category test case' do
+		before do
+			@test = Category.new(name: 'Family')
+		end
+
+		it "should have a name" do
+			expect(@test.name = 'family')
+		end
+	end
+end
+
+
+
+
+
+	expect()
+
+
+
+
+
+
+
+
+Per Ian, only really have code in the URL controller
+
+
+Categories controller
+Sessions (Login) controller
+User Register controller
+
+Slug should NOT be nil
+
+TDD style (write the test first, then write the code)
